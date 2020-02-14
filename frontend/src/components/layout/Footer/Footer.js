f0735a0d726982db76837f8bee8b1315f4267754
Mateusz Wszola <wszola.m@gmail.com>
2020-02-14 23:27:50 +0100

    Add footer component

---
+++ frontend/src/components/layout/Footer/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <div className="flex justify-center items-center h-screen bg-gray-900 text-gray-100">
+      <div className="">
+        <h2 className="text-3xl">Footer</h2>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
--- frontend/src/components/pages/Homepage.js
+++ frontend/src/components/pages/Homepage.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import LandingPage from '../layout/LandingPage';
 import About from '../layout/About';
+import { Footer } from '../layout/Footer';
 
 function Homepage() {
   return (
     <>
       <LandingPage />
       <About />
+      <Footer />
     </>
   );
 }
